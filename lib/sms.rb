d24e749a34136b73cfd5d41176c397d843513de7
Alex Satur <alex.satur89@gmail.com>
2017-10-12 07:59:08 +0100

    Update sms.rb

--- lib/sms.rb
+++ lib/sms.rb
@@ -4,14 +4,14 @@ class SMS
 
   def send_sms(order_id, name, cost, order)
 
-    account_sid = 'AC85d63b6b199aa2e579f511eaa1f74c44'
-    auth_token = '643f6a8a91ec054bb6bc1e8fdca02ac4'
+    account_sid = 'removed from github'
+    auth_token = 'removed from github'
 
     client = Twilio::REST::Client.new account_sid, auth_token
 
       client.messages.create(
-        from: '+441785472415',
-        to: '+447791415382',
+        from: '+removed from github',
+        to: '+removed from github',
         body: "Hi #{name}, Thanks for you order #{order_id} of #{order} totalling £#{cost}. Your food will be delivered in 1hr!"
       )
   end
